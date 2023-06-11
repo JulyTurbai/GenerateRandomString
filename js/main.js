9ba2ec274073943a300033c4040dc52a924cb808
July Turbay <iuliatsokur96@gmail.com>
2023-06-11 12:37:28 +0300

    made function

--- js/main.js
+++ js/main.js
@@ -8,42 +8,50 @@ const randomUp = ['A','B', 'C', 'D', 'E', 'F' ,'G' ,'H' ,'I' ,'J' ,'K' ,'L',
  const randomLow = ['a', 'b', 'c', 'd', 'e' ,'f', 'g','h' ,'i', 'j', 'k' ,'l' ,'m',
  'n', 'o', 'p' ,'q', 'r' ,'s' ,'t', 'u' ,'v', 'w' ,'x', 'y', 'z'];
  
-let num1 = 0;
+
+ let num1 = 0;
 
 
+generateRandomWords();
 
-$(document).ready(function() {
-    $("#number").change(function () {
-        num1 = Number($("#number").val());
+
+function generateRandomWords() {
+
+        $(document).ready(function() {
+        $("#number").change(function () {
+            num1 = Number($("#number").val());
+        });
+
+        $("button").click(function() {
+            let result = "";
+            let arr = [];
+
+            if(num1 == 0) {
+                alert('Enter string length!');
+            } else {
+                if($("#check1").is(":checked")) {
+                    arr = arr.concat(randomNum);
+                }
+                if($("#check2").is(":checked")) {
+                    arr = arr.concat(randomUp);
+                }
+                if($("#check3").is(":checked")) {
+                    arr = arr.concat(randomLow);
+                }
+
+                if(arr.length == 0) {
+                    alert('Choose characters for your string')
+                }
+                for (let i = 0; i < num1; i++) {
+                    result += arr[Math.floor(Math.random() * arr.length)];
+                }
+                $("#result").val(result)
+            }
+        });
     });
+}
 
-    $("button").click(function() {
-        let result = "";
-        let arr = [];
 
-        if(num1 == 0) {
-            alert('Enter string length!');
-        } else {
-            if($("#check1").is(":checked")) {
-                arr = arr.concat(randomNum);
-            }
-            if($("#check2").is(":checked")) {
-                arr = arr.concat(randomUp);
-            }
-            if($("#check3").is(":checked")) {
-                arr = arr.concat(randomLow);
-            }
-
-            if(arr.length == 0) {
-                alert('Choose characters for your string')
-            }
-            for (let i = 0; i < num1; i++) {
-                result += arr[Math.floor(Math.random() * arr.length)];
-            }
-            $("#result").val(result)
-        }
-    });
-});
 
 
 
